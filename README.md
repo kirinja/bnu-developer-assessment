3148e1d0b5c3dca187a0c61cfb40177ad8ef42ed
Niklas <blom.niklas@gmail.com>
2022-07-09 21:33:44 +0200

    switch to async flask and add async-route, no performance increase due to using a single worker

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 3. Installera python requirements med  ```pip install -r requirements.txt```
 4. Starta flask applikationen med ```flask run```
 5. Gå till [http://127.0.0.1:5000](http://127.0.0.1:5000)
+6. Gå till [http://127.0.0.1:5000/async-route](http://127.0.0.1:5000/async-route) för en async variant (ingen performance increase för flask är använder endast en worker, kan lösas genom att använda greenlet eller liknande)
--- app.py
+++ app.py
@@ -21,11 +21,25 @@ def feeds():
         for entry in item.entries:
             entries[entry.title] = entry
     
-    # sort on items and based on publishing date, parse the str to a datetime so we can properly sort
-    sorted_entries = sorted(entries.items(), key=lambda item: parse(item[1]['published']), reverse=True)
+    sorted_entries = sort_dict_on_published((entries))
 
     # grab latest 10 entries from all sources
     return [(entry[0], entry[1]['id']) for entry in sorted_entries[:10]]
+
+
+def sort_dict_on_published(items: dict, reverse:bool=True) -> dict:
+    # sort on items and based on publishing date, parse the str to a datetime so we can properly sort
+    sorted_items = sorted(items.items(), key=lambda item: parse(item[1]['published']), reverse=reverse)
+    return sorted_items
+
+
+async def parse_feed(feed_url: str) -> dict:
+    feed = feedparser.parse(feed_url)
+    entries = {}
+    for item in feed.entries:
+        entries[item.title] = item
+    
+    return entries
 
 
 @app.route("/", methods=["GET"])
@@ -33,3 +47,15 @@ def feeds():
 def index():
     top_ten = feeds()
     return render_template("index.html", entries=top_ten)
+
+
+@app.route("/async-route", methods=["GET"])
+async def async_route():
+    combined_entries = {}
+    with open("feeds.json") as obj:
+        temp = json.load(obj).get("feeds")
+    
+    for item in temp:
+        combined_entries.update(await parse_feed(item))
+    
+    return render_template("index.html", entries=sort_dict_on_published(combined_entries)[:10])
